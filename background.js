2d3fa841e0bc0ac4b83debe76aa2aa1f8eb18f4d
Alexander Galichenko <alexandr.galichenko@gmail.com>
2017-06-25 13:25:30 +0300

    fixed link duplication

--- background.js
+++ background.js
@@ -1,15 +1,17 @@
-var script = "document.getElementsByClassName(\"audio_page_player_ctrl audio_page_player_btns\")[0].innerHTML += '<a id=downloadLink download class=\"audio_page_player_btn audio_page_player_repeat _audio_page_player_repeat\" onclick=\"this.href=getAudioPlayer().getCurrentAudio()[2];\">Download</a>';";
+var script = "document.getElementsByClassName(\"audio_page_player_ctrl audio_page_player_btns\")[0].innerHTML += '<a id=downloadLink download class=\"downloadLink\" onclick=\"this.href=getAudioPlayer().getCurrentAudio()[2];\">Download</a>';";
 var checkDownloadLinkPresence = "document.getElementById(\"downloadLink\") === null";
 
-chrome.tabs.onUpdated.addListener(function(tab) {
-    chrome.tabs.executeScript({
-        code: checkDownloadLinkPresence
-    }, function (result) {
-        console.log(result);
-        if (result[0] === true) {
-            chrome.tabs.executeScript({
-                code: script
-            });
-        }
-    });
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (tab.status === "complete") {
+        chrome.tabs.executeScript({
+            code: checkDownloadLinkPresence
+        }, function (result) {
+            if (result[0] === true) {
+                chrome.tabs.executeScript({
+                    code: script
+                });
+            }
+        });
+    }
+
 });
